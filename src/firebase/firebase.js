126ef58039282e2b59068770ac256ea8214a9828
Kim Skogsmo <kim.skogsmo@gmail.com>
2021-07-04 20:20:17 +0200

    .

---
+++ src/firebase/firebase.js
@@ -0,0 +1,6 @@
+// // Firebase App (the core Firebase SDK) is always required and must be listed first
+// import { initializeApp } from 'firebase/app';
+
+// // Add the Firebase products that you want to use
+// import { getAuth } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
---
+++ src/firebase/init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgmCFjCdmv-MrY-gSPfCUA5CntGEbIQ5Q",
+    authDomain: "thinkfrontend.firebaseapp.com",
+    projectId: "thinkfrontend",
+    storageBucket: "thinkfrontend.appspot.com",
+    messagingSenderId: "343893954644",
+    appId: "1:343893954644:web:dae4bb274f6018c2019dc1"
+};
+  
+const firebaseApp = initializeApp(firebaseConfig);
+
